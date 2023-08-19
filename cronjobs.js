913ad8535305926861eb7ea1b70c50819e0db6dc
Navneet018 <navneet@logicfirst.io>
2023-08-20 04:52:52 +0530

    cronjob changes

---
+++ cronjobs.js
@@ -0,0 +1,77 @@
+const cron = require('node-cron');
+const orders = require('./models/orderModel');
+const { issueNFT } = require('./helpers/contractHelper');
+const { IMAGE_CONSTANTS, TOKEN_TYPE_MAPPING } = require('./constants');
+
+const generatePURCHASEToken = async () => {
+    console.log("Started process for Generating PURCHASETOKEN")
+    const task = cron.schedule('* * * * *', async () => {
+        try {
+            const filterObj = {
+
+                status: "done",
+                payment: {
+                    razorpay_order_id: { $exists: true },
+                    razorpay_payment_id: { $exists: true },
+                    razorpay_signature: { $exists: true }
+                },
+                tokenTransID: { $exists: false }
+
+            }
+            const orderArray = await orders.find(filterObj);
+            for (let i = 0; i < orderArray.length; i++) {
+                issueNFT(orderArray[i]?.addr, IMAGE_CONSTANTS.PURCHASE, TOKEN_TYPE_MAPPING.PURCHASE, orderArray[i]?.nftTokenValue, "abcdefghijk1234")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    task.start()
+}
+
+const generateSELLERCUSTOMERToken = async () => {
+    console.log("Started process for Generating SELLERCUSTOMER Token")
+    const task = cron.schedule('* * * * *', async () => {
+        try {
+            const filterObj = {
+
+                status: "done",
+                payment: {
+                    razorpay_order_id: { $exists: true },
+                    razorpay_payment_id: { $exists: true },
+                    razorpay_signature: { $exists: true }
+                },
+                tokenTransID: { $exists: false }
+
+            }
+            const orderArray = await orders.find(filterObj);
+            for (let i = 0; i < orderArray.length; i++) {
+                issueNFT(orderArray[i].addr, IMAGE_CONSTANTS.SELLER, TOKEN_TYPE_MAPPING.SELLER, orderArray[i]?.nftTokenValue, "123")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    task.start()
+}
+
+
+const generateREFERALToken = async () => {
+    console.log("Started process for Generating REFERAL Token")
+    const task = cron.schedule('* * * * *', async () => {
+        try {
+            const filterObj = {
+                kind: TOKENS.KYC,
+                docCaptured: true,
+                certCreated: false
+            }
+            const order = await orders.find(filterObj);
+            for (let i = 0; i < users.length; i++) {
+                // generateKYCPDF(users[i]?.toObject())
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    task.start()
+}
--- helpers/contractHelper.js
+++ helpers/contractHelper.js
@@ -3,66 +3,114 @@ const { CONSTANTS } = require("../constants");
 const { ABI } = require("../contracts/abis/customNFT");
 
 async function issueNFT(wallet, imageUrl, nftType, value, sellerId) {
-    console.log("entered");
+    try {
+        console.log("entered");
 
-    const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
-    const contractABI = ABI.CUSTOMNFT;
+        const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
+        const contractABI = ABI.CUSTOMNFT;
+    
+        const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
+    
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    
+        const signer = provider.getSigner();
+        const contractWithSigner = contract.connect(signer);
+    
+        const gasLimit = await contractWithSigner.estimateGas.issueNFT(wallet, imageUrl, nftType, value, sellerId);
+        const tx = await contractWithSigner.issueNFT(wallet, imageUrl, nftType, value, sellerId, { gasLimit });
+        console.log(tx);
+        
+    } catch (error) {
+        console.log(error)
+    }
 
-    const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
-
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-    const signer = provider.getSigner();
-    const contractWithSigner = contract.connect(signer);
-
-    const gasLimit = await contractWithSigner.estimateGas.issueNFT(wallet, imageUrl, nftType, value, sellerId);
-    const tx = await contractWithSigner.issueNFT(wallet, imageUrl, nftType, value, sellerId, { gasLimit });
-    console.log(tx);
 }
 
 async function getNFTDataByWallet(wallet) {
 
-    const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
-    const contractABI = ABI.CUSTOMNFT;
+    try {
+        const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
+        const contractABI = ABI.CUSTOMNFT;
+    
+        const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
+    
+    
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    
+    
+        const nftData = await contract.getNFTsByWallet(wallet);
+        console.log({ nftData });
+        return nftData;
+        
+    } catch (error) {
+        console.log(error)
+        return [];
+    }
 
-    const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
-
-
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-
-    const nftData = await contract.getNFTsByWallet(wallet);
-    console.log({ nftData });
-    return nftData;
 }
 
 async function getNFTDataByWalletExp(wallet) {
+    try {
+        const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
+        const contractABI = ABI.CUSTOMNFT;
+    
+        const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
+    
+    
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    
+        const nftDataExp = await contract.getNFTsByWalletExp(wallet);
+        console.log({ nftDataExp });
+        return nftDataExp;
+        
+    } catch (error) {
+        console.log(error)
+        return []
+    }
 
-    const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
-    const contractABI = ABI.CUSTOMNFT;
-
-    const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
-
-
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-    const nftDataExp = await contract.getNFTsByWalletExp(wallet);
-    console.log({ nftDataExp });
-    return nftDataExp;
+  
 }
 async function expireNFT(tokenID, wallet) {
+    try {
+        const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
+        const contractABI = ABI.CUSTOMNFT;
+    
+        const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
+    
+    
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    
+        const signer = provider.getSigner();
+        const contractWithSigner = contract.connect(signer);
+        await contractWithSigner.expireNFT(tokenID, wallet);
+        
+    } catch (error) {
+            console.log(error);
+    }
 
-    const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
-    const contractABI = ABI.CUSTOMNFT;
 
-    const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
-
-
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-    const signer = provider.getSigner();
-    const contractWithSigner = contract.connect(signer);
-    await contractWithSigner.expireNFT(tokenID, wallet);
 
 }
-module.exports = { issueNFT, getNFTDataByWallet, getNFTDataByWalletExp, expireNFT }
+
+async function autoEXPIRENFT(tokenID) {
+    try {
+        const provider = new ethers.providers.JsonRpcProvider(CONSTANTS["1337-RPC_URL"]);
+        const contractABI = ABI.CUSTOMNFT;
+    
+        const contractAddress = CONSTANTS["1337-CUSTOMNFT"];
+    
+    
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    
+        const signer = provider.getSigner();
+        const contractWithSigner = contract.connect(signer);
+        await contractWithSigner.autoExpireNFT(tokenID);
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
+}
+module.exports = { issueNFT, getNFTDataByWallet, getNFTDataByWalletExp, expireNFT, autoEXPIRENFT }
