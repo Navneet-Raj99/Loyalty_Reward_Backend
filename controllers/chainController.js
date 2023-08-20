cb7bbf4a7d9e9b2f3fa7a3b5d6492c1a8d74f884
Thinker-08 <agarwalmihir08@gmail.com>
2023-08-20 20:30:40 +0530

    Fixed User Referral system

--- controllers/chainController.js
+++ controllers/chainController.js
@@ -3,22 +3,40 @@ import _ from "lodash";
 
 export const setAddr = async (req, res ) =>
 {
-    const {signature, address, auth} = req.body;
-    console.log(signature, address, auth);
-    
-    // Code to unsign and match
+    try {
+        const {signature, address, auth} = req.body;
+        
+        // Code to unsign and match
 
-    const data = await userReferralModel.findAll({
-        referralCode: auth.user.refCode
-    });
-    if(!_.isEmpty(data)) {
-        const promises = data.map((e) => {
-            return userReferralModel.findOneAndUpdate({
-                referralCode: auth.user.refCode
-            },{
-                senderAddr: address
+        const data = await userReferralModel.find({
+            referralCode: auth.user.refCode,
+            senderUserId: auth.user._id
+        });
+        if(!_.isEmpty(data)) {
+            const promises = data.map((e) => {
+                return userReferralModel.updateMany({
+                    referralCode: auth.user.refCode,
+                    senderUserId: auth.user._id
+                },{
+                    senderAddr: address
+                })
             })
-        })
+            await Promise.all(promises);
+        }
+        const data2 = await userReferralModel.find({
+            consumerUserId: auth.user._id
+        });
+        const promises = data2.map((e) => {
+            return userReferralModel.updateMany({
+                consumerUserId: auth.user._id
+            }, {
+                receiverAddr: address 
+            })
+        });
         await Promise.all(promises);
+        res.status(200).send({success: true, message: "Successfully updated addresses"});
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({success: false, message: err});
     }
 }
